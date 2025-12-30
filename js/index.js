ff8829cec7098ae754d873f30a1fd923172e74c0
parsa <sample@sample.com>
2025-12-30 00:42:54 -0800

    fix

--- js/index.js
+++ js/index.js
@@ -5,16 +5,32 @@ const count_guess = document.getElementById("chance-count");
 
 let guess_number = Math.floor(Math.random() * 100) + 1;
 let count_chance = 0;
+let game_over = false;
 check_button.addEventListener("click" , compare);
 
 function compare() {
-    let user_number = parseInt(user_input.value);
+
+    
 
     if(game_over)
-    {
-        count_chance = 0;
 
-    }
+        {
+            game_over = false;
+            count_chance = 0;
+            check_button.innerText = "Check";
+            guess_number = Math.floor(Math.random() * 100) + 1;
+            hint.innerText = "I'm waiting for you!";
+            count_guess.innerText = count_chance;
+            user_input.disabled=false;
+            return;
+        }
+
+        let user_number = parseInt(user_input.value);
+        count_chance++;
+        count_guess.innerText = count_chance;
+
+        
+
 
 
     if(user_number > guess_number)
@@ -38,6 +54,7 @@ function compare() {
         user_input.disabled=true;
 
     }
-
-
+    user_input.value = "";
+    user_input.focus();
 }
+    
